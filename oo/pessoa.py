64b3fd096f7dcd1a8a20c8a5ff6a831c75c4f50f
viniciusfrantz <viniciusfrantz@hotmail.com>
2021-06-09 17:27:54 -0300

    Criada a Classe Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,2 @@
 class Pessoa:
-    def cumprimentar(self):
-        return f'Olá {id(self)}'
-
-if __name__ == '__main__':
-    p= Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
+    pass
