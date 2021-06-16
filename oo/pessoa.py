331b9edac9b7410f4be89fad175fb2f3b3301136
viniciusfrantz <viniciusfrantz@hotmail.com>
2021-06-16 17:03:37 -0300

    Criado atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,11 @@
 class Pessoa:
-    def __init__(self,*filhos, nome = None, idade=33):
+    olhos = 2
+
+    def __init__(self,*filhos, nome = None, idade=33,):
         self.idade = idade
         self.nome = nome
         self.filhos = list(filhos)
+        olhos=2
 
     def cumprimentar(self):
         return f'Olá {id(self)}{self}'
@@ -14,11 +17,18 @@ if __name__ == '__main__':
     print(id(antonio))
     print(antonio.cumprimentar())
     print(antonio.nome)
-    print(antonio''.idade)
+    print(antonio.idade)
     for filho in antonio.filhos:
         print(filho.nome)
     antonio.sobrenome = 'Giacomini'
     del antonio.filhos
+    vinicius.olhos = 1
+    del vinicius.olhos
     print(antonio.__dict__)
     print(vinicius.__dict__)
-    print(antonio.sobrenome)
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(vinicius.olhos)
+    print(antonio.olhos)
+
+    print(id(Pessoa.olhos), id(antonio.olhos), id(vinicius.olhos))
