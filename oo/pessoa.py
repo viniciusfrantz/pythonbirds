527de2788217048156eeeed21c25e5567ff10d36
viniciusfrantz <viniciusfrantz@hotmail.com>
2021-06-16 16:50:18 -0300

    Criado e removido atributo dinâmico de objetos do tipo Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -14,6 +14,11 @@ if __name__ == '__main__':
     print(id(antonio))
     print(antonio.cumprimentar())
     print(antonio.nome)
-    print(antonio.idade)
+    print(antonio''.idade)
     for filho in antonio.filhos:
         print(filho.nome)
+    antonio.sobrenome = 'Giacomini'
+    del antonio.filhos
+    print(antonio.__dict__)
+    print(vinicius.__dict__)
+    print(antonio.sobrenome)
